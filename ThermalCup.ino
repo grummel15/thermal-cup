f2b0574050fcea111ff111d5a8caf84c209b2ced
grummel15 <36243886+grummel15@users.noreply.github.com>
2018-04-24 16:27:31 -0400

    Add files via upload

---
+++ ThermalCup.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int Temp = analogRead(37);
+  float Voltage = Temp * (5/2048.0);
+  float Kelvin = Voltage* (1/.01);
+  float Celsius = Kelvin - 303.15;
+  Serial.println(Celsius);
+  delay(1000);
+  if (Celsius = 40)  digitalWrite(5,HIGH);
+  if (Celsius != 40)  digitalWrite (5,LOW);
+
+}
